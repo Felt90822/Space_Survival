fa3c5fc645ea75534c85a627e067c2e199995ece
Felt90822 <104243052+Felt90822@users.noreply.github.com>
2023-03-22 22:00:31 +0800

    Add files via upload

---
+++ Main/Object.py
@@ -0,0 +1,79 @@
+import pygame
+import random
+
+WIDTH = 900
+HEIGHT = 900
+
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+
+#玩家
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT - 10
+        self.speed = 7
+
+    def update(self):
+        key_pressed = pygame.key.get_pressed() #回傳使用者按的按鍵
+        if key_pressed[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        if key_pressed[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if key_pressed[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if key_pressed[pygame.K_DOWN]:
+            self.rect.y += self.speed
+
+        if self.rect.right > WIDTH:  #讓飛船不會跑出界
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+#俗投
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-180, -150)
+        self.speedy = random.randrange(2, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right < 0:
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(2, 10)  #速度
+            self.speedx = random.randrange(-3, 3)
+
+#子彈
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
